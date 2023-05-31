8735b54ceb8ee17015eea43b666280f6c55d49ee
Gabriel Gigante <q1dcc21@gmail.com>
2023-05-31 11:53:53 -0300

    ADD many things 😁

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-function toggle(title = 'Error', about = 'Error') {
-    document.getElementById('popup-title').innerHTML = title;
-    document.getElementById('popup-about').innerHTML = about;
-    document.getElementById('popup').classList.toggle('active');
+function toggle(title = "Error", about = "Error") {
+  document.getElementById("popup-title").innerHTML = title;
+  document.getElementById("popup-about").innerHTML = about;
+  document.getElementById("popup").classList.toggle("active");
 }
+
